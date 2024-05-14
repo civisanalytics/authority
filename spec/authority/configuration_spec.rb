78097d966e59ca541815cec341c36ba895ae4109
Gabe Mendoza <gmendoza@civisanalytics.com>
2024-05-14 16:26:15 -0500

    pretty confident this should pass for all versions

--- spec/authority/configuration_spec.rb
+++ spec/authority/configuration_spec.rb
@@ -15,7 +15,8 @@ describe Authority::Configuration do
 
       it "logs to standard error by default" do
         Authority.instance_variable_set :@configuration, nil
-        null = File.exists?('/dev/null') ? '/dev/null' : 'NUL:' # Allow for Windows
+        method = RUBY_VERSION < '3.2.0' ? :exists? : :exist?
+        null = File.send(method, '/dev/null') ? '/dev/null' : 'NUL:' # Allow for Windows
         logger = Logger.new(null)
         expect(Logger).to receive(:new).with(STDERR).and_return(logger)
         Authority.configure
